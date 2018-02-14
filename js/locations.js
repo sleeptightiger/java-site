6b1f9ff55f957d43aa66244b81651071f91290bc
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-02-14 17:08:02 -0600

    map with pointers

--- js/locations.js
+++ js/locations.js
@@ -1,12 +1,11 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,6 +13,7 @@ function currentSlide(n) {
 function showSlides(n) {
   const slides = document.getElementsByClassName("mySlides");
   const dots = document.getElementsByClassName("dot");
+  const pointers = document.getElementsByClassName("pointer");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (let i = 0; i < slides.length; i++) {
@@ -22,6 +22,12 @@ function showSlides(n) {
   for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  for(let i = 0; i < pointers.length; i++) {
+    pointers[i].className = pointers[i].className.replace(" selected", "");
+    pointers[i].setAttribute("src", "img/blackpointer.svg");
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  pointers[slideIndex-1].className += " selected";
+  pointers[slideIndex-1].setAttribute("src", "img/orangepointer.svg");
 }
