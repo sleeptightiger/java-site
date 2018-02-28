f4fa4abd4a9110dce5f0788ef5d1c07f9fc3e82e
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-02-28 12:46:33 -0600

    fix footer nav hover

--- js/about.js
+++ js/about.js
@@ -1,5 +1,8 @@
 const photos = document.querySelectorAll(".img .overlay");
 const text = document.querySelectorAll(".text");
+const card = document.querySelectorAll(".card");
+
+
 
 for(let i = 0;i < photos.length; i++) {
   photos[i].addEventListener('mouseover', (e) => {
@@ -17,3 +20,64 @@ for(let i = 0;i < photos.length; i++) {
     photos[i].classList.remove('clear');
   });
 }
+
+
+findHeights = (elements) => {
+  const heights = [];
+  for(let i = 0; i < elements.length; i++) {
+    const children = elements[i].children;
+    let height = 0;
+    for(let j = 0; j < children.length; j++) {
+      height += children[j].clientHeight;
+    }
+    heights.push(height);
+  }
+  return heights;
+}
+
+findMax = (heights) => {
+  let max = 0;
+  for(let i = 0; i < heights.length; i++) {
+    if(max < heights[i]) {
+      max = heights[i];
+    }
+  }
+  return max;
+}
+
+findMin = (heights) => {
+  let min = heights[0];
+  for(let i = 0; i < heights.length; i++) {
+    if(min > heights[i]) {
+      min = heights[i];
+    }
+  }
+  return min;
+}
+
+setHeight = (elements, height) => {
+  let thisHeight = height + 70;
+  for(let i = 0; i < elements.length; i++) {
+    let set = 'height:' + thisHeight + 'px;';
+    elements[i].setAttribute('style', set);
+  }
+}
+
+
+
+// const width = window.innerWidth;
+// if(width > 768) {
+//   const heights = findHeights(text);
+//   const maxHeight = findMax(heights);
+//   setHeight(card, maxHeight);
+// }
+//
+// window.addEventListener('resize', (e) => {
+//   const width = window.innerWidth;
+//   if(width > 768) {
+//     const heights = findHeights(text);
+//     const maxHeight = findMax(heights);
+//     setHeight(card, maxHeight);
+//   }
+//
+// });
