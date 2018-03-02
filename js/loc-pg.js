e58ce65ecce431c5539b134c2c36f09c48e8bee8
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-03-02 14:59:17 -0600

    location responsive

--- js/loc-pg.js
+++ js/loc-pg.js
@@ -1,2 +1,27 @@
 const photos = document.querySelectorAll('.info-img');
-const text = document.querySelectorAll('.details');
+const textDetails = document.querySelectorAll('.details');
+const hoverText = document.querySelectorAll('.hover-text');
+const infoOverlay = document.querySelectorAll('.info-overlay');
+
+
+
+for(let i = 0;i < photos.length; i++) {
+  photos[i].addEventListener('mouseover', (e) => {
+    infoOverlay[i].classList.toggle('hover');
+    hoverText[i].classList.toggle('move-in');
+  });
+
+  photos[i].addEventListener('mouseout', (e) => {
+    infoOverlay[i].classList.remove('hover');
+    hoverText[i].classList.toggle('move-in');
+  });
+  textDetails[i].addEventListener('mouseover', (e) => {
+    infoOverlay[i].classList.toggle('hover');
+    hoverText[i].classList.toggle('move-in');
+  });
+
+  textDetails[i].addEventListener('mouseout', (e) => {
+    infoOverlay[i].classList.remove('hover');
+    hoverText[i].classList.toggle('move-in');
+  });
+}
