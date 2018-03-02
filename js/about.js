f38ff306d4ef92a0071b3be6a27f9fd8e08891a9
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-03-01 19:44:51 -0600

    view location text add below 480px

--- js/about.js
+++ js/about.js
@@ -4,21 +4,25 @@ const card = document.querySelectorAll(".card");
 
 
 
-for(let i = 0;i < photos.length; i++) {
-  photos[i].addEventListener('mouseover', (e) => {
-    photos[i].classList.toggle('clear');
-  });
 
-  photos[i].addEventListener('mouseout', (e) => {
-    photos[i].classList.remove('clear');
-  });
-  text[i].addEventListener('mouseover', (e) => {
-    photos[i].classList.toggle('clear');
-  });
 
-  text[i].addEventListener('mouseout', (e) => {
-    photos[i].classList.remove('clear');
-  });
+hoverOverlayToggle = (photos, text, hover) => {
+  for(let i = 0;i < photos.length; i++) {
+    photos[i].addEventListener('mouseover', (e) => {
+      photos[i].classList.toggle(hover);
+    });
+
+    photos[i].addEventListener('mouseout', (e) => {
+      photos[i].classList.remove(hover);
+    });
+    text[i].addEventListener('mouseover', (e) => {
+      photos[i].classList.toggle(hover);
+    });
+
+    text[i].addEventListener('mouseout', (e) => {
+      photos[i].classList.remove(hover);
+    });
+  }
 }
 
 
@@ -79,7 +83,7 @@ evenHeights = (className) => {
 
 
 
-
+hoverOverlayToggle(photos, text, 'clear');
 evenHeights("card");
 
 
