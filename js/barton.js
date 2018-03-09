4893c9d01424b105ee4952f8e30a11d86c07f3c6
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-03-09 13:07:08 -0600

    add schedule js

--- js/barton.js
+++ js/barton.js
@@ -14,9 +14,36 @@ window.addEventListener('resize', (e) => {
   if(width > 1024) {
     schedule.removeChild(headline);
     menus.insertBefore(headline, menus.childNodes[0]);
-  } else {
+  } else if (width < 1024) {
     menus.removeChild(headline);
     schedule.insertBefore(headline, schedule.childNodes[0]);
   }
 
 });
+
+
+const d = new Date();
+const weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let n = weekday[d.getDay()];
+console.log(n);
+
+const day = document.querySelectorAll(`.${n}`);
+const clock = document.querySelector('.clock');
+let margin;
+if(d.getDay() == 0) {
+  margin = 30*(6) + 5;
+} else {
+  margin = 30*(d.getDay() - 1) + 5;
+}
+console.log(margin);
+clock.setAttribute('style', `margin-top: ${margin}px;`);
+day[0].setAttribute('style', 'color: #000;');
+day[1].setAttribute('style', 'color: #000;');
