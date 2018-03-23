d18be567a487e6aad3dbd13506929e4da6700cef
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-03-23 17:17:02 -0500

    fix jump in sticky header

--- js/header.js
+++ js/header.js
@@ -2,11 +2,13 @@ const header = document.querySelector("header");
 const headContainer = header.querySelector(".head-container");
 const logo = header.querySelector(".logo-withstar img");
 const body = document.querySelector("body");
+const indexBody = document.querySelector(".index");
+const diagonal = document.querySelector(".diagonal-bg");
 
 
 const height = header.offsetHeight;
 
-console.log(height);
+console.log(indexBody);
 
 
 // When the user scrolls the page, execute myFunction
@@ -15,19 +17,29 @@ window.onscroll = function() {sticky()};
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function sticky() {
-  if (window.pageYOffset >= height) {
+  if (window.pageYOffset > height) {
     header.classList.add("sticky");
     headContainer.classList.add("sticky-head");
     logo.style.height = "60px";
     logo.style.width = "65px";
     logo.style.margin = "5px 0";
-    body.style.top = "70px";
+    console.log(modalOpen);
+    if(indexBody == null) {
+      body.style.marginTop = "86px";
+    } else {
+      body.style.marginTop = "57px";
+      diagonal.style.height = "665px"
+    }
+
+
+    // header.style.display = "none";
   } else {
     header.classList.remove("sticky");
     headContainer.classList.remove("sticky-head");
     logo.style.height = "98px";
     logo.style.width = "103px";
     logo.style.margin = "0";
-    body.style.top = "0px";
+    body.style.marginTop = "0px";
+    diagonal.style.height = "780px"
   }
 }
