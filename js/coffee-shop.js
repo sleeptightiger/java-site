f1857e51e1266466c4fa8d6572313c7826743d47
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-03-27 13:23:13 -0500

    responsive slanted bg

--- js/coffee-shop.js
+++ js/coffee-shop.js
@@ -1,18 +1,20 @@
 const headline = document.querySelector('.coffee-shop-headline');
 width = window.innerWidth;
+const diagonalBg = document.querySelector('.bg-lower-store');
 if(width < 768) {
   headline.classList.add("horizontal-line-headline");
   headline.style.marginTop = "0px";
+  diagonalBg.style.display = 'none';
 
 }
 window.addEventListener('resize', (e) => {
   width = window.innerWidth;
   if(width < 768) {
     headline.classList.add("horizontal-line-headline");
-
+    diagonalBg.style.display = 'none';
   } else {
     headline.classList.remove("horizontal-line-headline");
-
+    diagonalBg.style.display = 'block';
   }
 
 });
